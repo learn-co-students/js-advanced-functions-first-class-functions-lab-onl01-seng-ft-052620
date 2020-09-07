6381f003b44c7ff12ec7bb7df58521f51806a678
Jonathan Bleibdrey <jonathanbleibdrey@Jonathans-MacBook-Air-2.local>
2020-09-07 15:23:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0, 2)
+}
+
+function returnLastTwoDrivers(drivers){
+    return drivers.slice(2,5)
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(integers){
+    return function(integer){
+        return integer * integers
+    }
+
+}
+
+const fareDoubler = function(fare){
+    return createFareMultiplier(2)(fare)
+}
+
+const fareTripler = function(fare){
+    return createFareMultiplier(3)(fare)
+}
+
+function selectDifferentDrivers(drivers, selecMethod){
+    return selecMethod(drivers)
+}
